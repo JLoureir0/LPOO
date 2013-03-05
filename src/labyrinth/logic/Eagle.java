e42c10ceec678301be355d3777950d1d23144155
JLoureiro <joao.a.g.loureiro@gmail.com>
2013-03-05 15:38:02 +0000

    Adicionada class Eagle

---
+++ src/labyrinth/logic/Eagle.java
@@ -0,0 +1,20 @@
+package labyrinth.logic;
+
+public class Eagle extends Character{
+
+	
+	public Eagle(int x, int y, char symbol) {
+		super(x, y, symbol);
+	}
+
+
+	public boolean hasSpade() {
+		
+		return false;
+	}
+
+	
+	public void handleSpade() {		
+	}
+
+}
